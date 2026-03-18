b7c8d191e852e80040965d326fcf54b8d7c06d86
SakethPeri <157396901+SakethPeri@users.noreply.github.com>
2026-03-18 11:01:53 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "tm4c123gh6pm.h"
 #include "clock.h"
 #include "gpio.h"
-#include "wait.h"     // Make sure wait.h exists and has waitMicrosecond!
+#include "wait.h"     
 #include "printstr.h"
 
 void initHw() {
